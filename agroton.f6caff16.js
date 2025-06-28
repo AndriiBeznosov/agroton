ec262b405581d3ef93d83ba57c348cb50fca12a8
AndriiBeznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2025-06-28 18:15:10 +0000

    Deploying to gh-pages from @ AndriiBeznosov/agroton@8b19f1c03f98e79f17e4045f87694d66dc405143 🚀

--- agroton.f6caff16.js
+++
@@ -1,2 +0,0 @@
-let btn=document.querySelector(".menu__btn").addEventListener("click",()=>{document.querySelector(".menu__list").classList.toggle("menu--active")});
-//# sourceMappingURL=agroton.f6caff16.js.map
